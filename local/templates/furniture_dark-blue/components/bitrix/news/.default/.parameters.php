7d898481d88989798754393b6d5f0fd3c0196ab5
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-04 14:32:25 +0300

    feat: add parameters ID_INFO_BLOCK

--- local/templates/furniture_dark-blue/components/bitrix/news/.default/.parameters.php
+++ local/templates/furniture_dark-blue/components/bitrix/news/.default/.parameters.php
@@ -2,25 +2,10 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 $arTemplateParameters = array(
-	"DISPLAY_DATE" => array(
-		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_DATE"),
-		"TYPE" => "CHECKBOX",
-		"DEFAULT" => "Y",
-	),
-	"DISPLAY_NAME" => array(
-		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_NAME"),
-		"TYPE" => "CHECKBOX",
-		"DEFAULT" => "Y",
-	),
-	"DISPLAY_PICTURE" => array(
-		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_PICTURE"),
-		"TYPE" => "CHECKBOX",
-		"DEFAULT" => "Y",
-	),
-	"DISPLAY_PREVIEW_TEXT" => array(
-		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS_TEXT"),
-		"TYPE" => "CHECKBOX",
-		"DEFAULT" => "Y",
+
+	"ID_INFO_BLOCK" => array(
+		"NAME" => GetMessage("ID_INFO_BLOCK"),
+		"TYPE" => "STRING",
 	),
 
 );
---
+++ local/templates/furniture_dark-blue/components/bitrix/news/.default/lang/ru/.parameters.php
@@ -0,0 +1,2 @@
+<?php
+$MESS['ID_INFO_BLOCK'] = "ID информационного блока для rel=canonical";
