4c19d06cb3d46cb45fab66806808713a996b75d7
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-09 09:53:26 +0300

    fix: rename PROPERTY_NEW => PROPERY_NEWS

--- local/templates/furniture_dark-blue/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/furniture_dark-blue/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
@@ -2,13 +2,13 @@
 
 if (!empty($arParams["ID_INFO_BLOCK_CANONICAL"])) {
 
-  $arSelect = ["ID", "IBLOCK_ID", "NAME", "PROPERTY_NEW"];
+  $arSelect = ["ID", "IBLOCK_ID", "NAME", "PROPERTY_NEWS"];
 
-  $arFilter = ["IBLOCK_ID" => $arParams["ID_INFO_BLOCK_CANONICAL"], "PROPERTY_NEW" => $arResult["ID"], "ACTIVE" => "Y"];
+  $arFilter = ["IBLOCK_ID" => $arParams["ID_INFO_BLOCK_CANONICAL"], "PROPERTY_NEWS" => $arResult["ID"], "ACTIVE" => "Y"];
 
   $res = CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
 
-  if ($ob = $res->GetNextElement()) {
+  while ($ob = $res->GetNextElement()) {
     $arFields = $ob->GetFields();
     $arResult["CANONICAL_NAME"] = $arFields["NAME"];
     $this->__component->setResultCacheKeys(["CANONICAL_NAME"]);
