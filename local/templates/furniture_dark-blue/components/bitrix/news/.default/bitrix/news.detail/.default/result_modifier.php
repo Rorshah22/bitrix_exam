0d767f40d3076b500dde43b052c1805371646c16
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-11 12:07:28 +0300

    fix: replace GetNextElement on Fetch

--- local/templates/furniture_dark-blue/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/furniture_dark-blue/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
@@ -2,15 +2,14 @@
 
 if (!empty($arParams["ID_INFO_BLOCK_CANONICAL"])) {
 
-  $arSelect = ["ID", "IBLOCK_ID", "NAME", "PROPERTY_NEWS"];
+  $arSelect = ["ID", "IBLOCK_ID", "NAME"];
 
   $arFilter = ["IBLOCK_ID" => $arParams["ID_INFO_BLOCK_CANONICAL"], "PROPERTY_NEWS" => $arResult["ID"], "ACTIVE" => "Y"];
 
   $res = CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
 
-  while ($ob = $res->GetNextElement()) {
-    $arFields = $ob->GetFields();
-    $arResult["CANONICAL_NAME"] = $arFields["NAME"];
+  if ($ob = $res->Fetch()) {
+    $arResult["CANONICAL_NAME"] = $ob["NAME"];
     $this->__component->setResultCacheKeys(["CANONICAL_NAME"]);
   }
 }
