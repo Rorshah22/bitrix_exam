35a794076d670ed21b965d6a22cb8bf51dbf76ec
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-14 17:58:54 +0300

    fix: edit method GetMessage

--- local/components/ex2/simplecomp.exam/.parameters.php
+++ local/components/ex2/simplecomp.exam/.parameters.php
@@ -1,19 +1,22 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+use \Bitrix\Main\Localization\Loc;
+
 $arComponentParameters = array(
 	"PARAMETERS" => array(
 		"PRODUCTS_IBLOCK_ID" => [
-			"NAME" => GetMessage("SIMPLECOMP_EXAM2_CAT_IBLOCK_ID"),
+			"NAME" => Loc::getMessage("SIMPLECOMP_EXAM2_CAT_IBLOCK_ID"),
 			"PARENT" => "BASE",
 			"TYPE" => "STRING",
 		],
 		"NEWS_IBLOCK_ID" => [
-			"NAME" => GetMessage("NEWS_IBLOCK_ID"),
+			"NAME" => Loc::getMessage("NEWS_IBLOCK_ID"),
 			"PARENT" => "BASE",
 			"TYPE" => "STRING",
 		],
 		"CODE_USER_PROPERTY" => [
-			"NAME" => GetMessage('CODE_USER_PROPERTY'),
+			"NAME" => Loc::getMessage('CODE_USER_PROPERTY'),
 			"PARENT" => "BASE",
 			"TYPE" => 'STRING'
 		],
