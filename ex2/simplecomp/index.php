d53bd4e4be3f37ab6c41c790111fc3295b2a9cc1
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-14 16:34:03 +0300

    feat:add component on page

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -2,9 +2,15 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Простой компонент");
 ?>Text here....<?$APPLICATION->IncludeComponent(
-	"ex2:simplecomp.exam",
-	"",
-	Array(
-		"PRODUCTS_IBLOCK_ID" => ""
-	)
+	"ex2:simplecomp.exam", 
+	".default", 
+	array(
+		"PRODUCTS_IBLOCK_ID" => "",
+		"COMPONENT_TEMPLATE" => ".default",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"NEWS_IBLOCK_ID" => "",
+		"CODE_USER_PROPERTY" => ""
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
