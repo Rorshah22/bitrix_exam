fde6f98f8dbfe62acca557c171cdcd00fe03c4e1
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-14 17:59:43 +0300

    fix: onPrepareComponentParams

--- local/components/ex2/simplecomp.exam/class.php
+++ local/components/ex2/simplecomp.exam/class.php
@@ -1,20 +1,34 @@
 <?php
-
 if (!defined("B_PROLOG_INCLUDED") && B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main\Loader;
 
 class SimpleComp extends CBitrixComponent
 {
 
   public function onPrepareComponentParams($arParams)
   {
-    $result = [];
+    echo '<pre>';
     var_dump($arParams);
+    $result = [
+      "PRODUCTS_IBLOCK_ID" => trim($arParams["PRODUCTS_IBLOCK_ID"]),
+      "NEWS_IBLOCK_ID" => trim($arParams["NEWS_IBLOCK_ID"]),
+      "CODE_USER_PROPERTY" => $arParams["CODE_USER_PROPERTY"],
+    ];
     return $result;
   }
 
 
   public function executeComponent()
   {
+    if (!Loader::includeModule("iblock")) {
+      ShowError(GetMessage("SIMPLECOMP_EXAM2_IBLOCK_MODULE_NONE"));
+      return;
+    }
+
+    global $APPLICATION;
+    $APPLICATION->SetTitle("В каталоге товаров представлено товаров: $[Количество]");
+    $this->arResult['d'] = ["fsd" => 'fsdf'];
     $this->includeComponentTemplate();
   }
 }
