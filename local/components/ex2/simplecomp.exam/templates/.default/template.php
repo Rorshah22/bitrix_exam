60e93e13ba66137fdfc16c6e49cdb21130c94f62
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-16 12:09:17 +0300

    fix: add unput component on page

--- local/components/ex2/simplecomp.exam/templates/.default/template.php
+++ local/components/ex2/simplecomp.exam/templates/.default/template.php
@@ -1,7 +1,37 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-<p><b><?= GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE") ?></b></p>
-<pre>
-<?
-print_r($arResult);
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main\Localization\Loc;
 ?>
-</pre>
+<p><b><?= Loc::getMessage("SIMPLECOMP_EXAM2_CAT_TITLE") ?></b></p>
+
+
+<? foreach ($arResult["NEWS"] as $key => $res) : ?>
+  <ul>
+    <li><b> <?= $res["NAME"] ?> </b> - <?= $res["ACTIVE_FROM"] ?>
+      (
+      <? $i = '';
+      foreach ($res["SECTION"] as $section) {
+        echo  $i . $section["NAME_SECTION"];
+        $i = ", ";
+      }
+      ?>
+      )
+      <ul>
+        <? foreach ($res["ITEM"] as $arItem) :  ?>
+          <? foreach ($arItem as $item) :  ?>
+            <li>
+              <?= $item["NAME"] ?> -
+              <?= $item["PROPERTY_PRICE_VALUE"] ?> -
+              <?= $item["PROPERTY_MATERIAL_VALUE"] ?> -
+              <?= $item["PROPERTY_ARTNUMBER_VALUE"] ?>
+            </li>
+          <? endforeach; ?>
+        <? endforeach; ?>
+
+      </ul>
+    </li>
+  </ul>
+
+<? endforeach; ?>
+<!-- <? echo "<pre>";
+      print_r($arResult); ?> -->
