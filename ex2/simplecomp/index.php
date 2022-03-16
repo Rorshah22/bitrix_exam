3c72afdc0fd23aea5ca9dac454a8f7069a576ab2
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-16 12:06:01 +0300

    fix: edit unput data

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -1,16 +1,16 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Простой компонент");
-?>Text here....<?$APPLICATION->IncludeComponent(
+?><?$APPLICATION->IncludeComponent(
 	"ex2:simplecomp.exam", 
 	".default", 
 	array(
-		"PRODUCTS_IBLOCK_ID" => "",
-		"COMPONENT_TEMPLATE" => ".default",
-		"CACHE_TYPE" => "A",
 		"CACHE_TIME" => "36000000",
-		"NEWS_IBLOCK_ID" => "",
-		"CODE_USER_PROPERTY" => ""
+		"CACHE_TYPE" => "A",
+		"CODE_USER_PROPERTY" => "UF_NEWS_LINK",
+		"COMPONENT_TEMPLATE" => ".default",
+		"NEWS_IBLOCK_ID" => "1",
+		"PRODUCTS_IBLOCK_ID" => "2"
 	),
 	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
