f5cb1d7c5eac3fd97b0dc008f69a6891ab7f4d4b
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-16 12:07:59 +0300

    fix: arResult formation and set title

--- local/components/ex2/simplecomp.exam/class.php
+++ local/components/ex2/simplecomp.exam/class.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") && B_PROLOG_INCLUDED !== true) die();
 
+
 use Bitrix\Main\Loader;
 
 class SimpleComp extends CBitrixComponent
@@ -8,16 +9,123 @@ class SimpleComp extends CBitrixComponent
 
   public function onPrepareComponentParams($arParams)
   {
-    echo '<pre>';
-    var_dump($arParams);
     $result = [
-      "PRODUCTS_IBLOCK_ID" => trim($arParams["PRODUCTS_IBLOCK_ID"]),
-      "NEWS_IBLOCK_ID" => trim($arParams["NEWS_IBLOCK_ID"]),
+      "PRODUCTS_IBLOCK_ID" => $arParams["PRODUCTS_IBLOCK_ID"] !== '' ? trim($arParams["PRODUCTS_IBLOCK_ID"]) : 0,
+      "NEWS_IBLOCK_ID" => $arParams["NEWS_IBLOCK_ID"] !== "" ? trim($arParams["NEWS_IBLOCK_ID"]) : 0,
       "CODE_USER_PROPERTY" => $arParams["CODE_USER_PROPERTY"],
     ];
     return $result;
   }
 
+  private  function  getSectionProduct()
+  {
+
+    if (CModule::IncludeModule('iblock')) {
+
+      $arFilter = [
+        "IBLOCK_ID" => $this->arParams["PRODUCTS_IBLOCK_ID"],
+        "ACTIVE" => "Y"
+      ];
+      $section = CIBlockSection::GetList(
+        [],
+        $arFilter,
+        false,
+        ["UF_NEWS_LINK"]
+      );
+      while ($arSection = $section->Fetch()) {
+        $arResult[]  = [
+          "ID" => $arSection["ID"],
+          "NAME" => $arSection["NAME"],
+          "UF_NEWS_LINK" => $arSection["UF_NEWS_LINK"]
+        ];
+      }
+    }
+    return $arResult;
+  }
+
+  private function getElementsProduct()
+  {
+    if (CModule::IncludeModule('iblock')) {
+
+      $arFilter = [
+        "IBLOCK_ID" => $this->arParams["PRODUCTS_IBLOCK_ID"],
+        "ACTIVE" => "Y"
+      ];
+      $arSelect = ["ID", "IBLOCK_ID", "IBLOCK_SECTION_ID", "NAME", "PROPERTY_MATERIAL", "PROPERTY_ARTNUMBER", "PROPERTY_PRICE"];
+      $res = CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
+      while ($ar = $res->Fetch()) {
+        $arResult[] = $ar;
+      }
+    }
+    return $arResult;
+  }
+
+  private function getElementsNews()
+  {
+
+    if (CModule::IncludeModule('iblock')) {
+      $arFilter = [
+        "IBLOCK_ID" => $this->arParams["NEWS_IBLOCK_ID"],
+        "ACTIVE" => "Y"
+      ];
+      $arSelect = ["ID", "IBLOCK_ID", "NAME", "ACTIVE_FROM"];
+
+      $res = CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
+
+      while ($ar = $res->Fetch()) {
+        $arResult[] = $ar;
+      }
+    }
+
+    return $arResult;
+  }
+
+  private function resultFormation()
+  {
+    $sectionsProduct = $this->getSectionProduct();
+    $elementsProduct  = $this->getElementsProduct();
+    $elementsNews  = $this->getElementsNews();
+
+    //записываем количество элементов в arResult
+    $this->arResult["COUNT"] = count($elementsProduct);
+
+
+    //запись элементов в соответствующие разделы
+    foreach ($elementsProduct as  $element) {
+
+      foreach ($sectionsProduct as $key => $section) {
+        if ($section["ID"] == $element["IBLOCK_SECTION_ID"]) {
+
+          $sectionsProduct[$key]['ITEM'][] =  $element;
+        }
+      }
+    }
+    //запись в массив соответствующих новостей и разделов 
+    $newsList = [];
+    foreach ($elementsNews as $news) {
+
+      foreach ($sectionsProduct as $section) {
+
+        if (in_array($news["ID"], $section["UF_NEWS_LINK"])) {
+          $arSection[] = [
+            "ID" => $section["ID"],
+            "NAME_SECTION" => $section["NAME"]
+          ];
+          $arItem[] = $section["ITEM"];
+          $newsList[$news["ID"]] = [
+            "ID" => $news["ID"],
+            "NAME" => $news["NAME"],
+            "ACTIVE_FROM" => $news["ACTIVE_FROM"],
+            "SECTION" => $arSection, "ITEM" => $arItem
+          ];
+        }
+      }
+      unset($arItem);
+      unset($arSection);
+    }
+
+    return $newsList;
+  }
 
   public function executeComponent()
   {
@@ -26,9 +134,17 @@ class SimpleComp extends CBitrixComponent
       return;
     }
 
-    global $APPLICATION;
-    $APPLICATION->SetTitle("В каталоге товаров представлено товаров: $[Количество]");
-    $this->arResult['d'] = ["fsd" => 'fsdf'];
-    $this->includeComponentTemplate();
+    if ($this->startResultCache()) {
+
+      $this->arResult["NEWS"] = $this->resultFormation();
+      $countItems = $this->arResult["COUNT"];
+
+      global $APPLICATION;
+      $APPLICATION->SetTitle(GetMessage("COUNT", ["#CNT#" => $countItems]));
+
+      $this->includeComponentTemplate();
+    } else {
+      $this->abortResultCache();
+    }
   }
 }
