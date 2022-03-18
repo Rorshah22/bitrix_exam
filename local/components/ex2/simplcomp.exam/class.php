6e8a7f71b21c1775ee33d0716ab43d1f883bb847
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-18 13:46:41 +0300

    feat: unput array news

--- local/components/ex2/simplcomp.exam/class.php
+++ local/components/ex2/simplcomp.exam/class.php
@@ -13,6 +13,7 @@ class SimpleComp extends CBitrixComponent
       "NEWS_IBLOCK_ID" => !empty($arParams["NEWS_IBLOCK_ID"]) ? intval(trim($arParams["NEWS_IBLOCK_ID"])) : 0,
       "AUTHOR" => $arParams["AUTHOR"],
       "TYPE_AUTHOR" => $arParams["TYPE_AUTHOR"],
+      "CACHE_TIME" => !empty($arParams["CACHE_TIME"]) ? $arParams["CACHE_TIME"] : 36000000,
     ];
   }
 
@@ -25,7 +26,8 @@ class SimpleComp extends CBitrixComponent
 
     while ($arr = $userList->Fetch()) {
 
-      $result[] = ["ID" => $arr["ID"], "LOGIN" => $arr["LOGIN"], "UF_AUTHOR_TYPE" => $arr["UF_AUTHOR_TYPE"]];
+      $result["LIST_AUTHOR"][] = ["ID" => $arr["ID"], "LOGIN" => $arr["LOGIN"], "UF_AUTHOR_TYPE" => $arr["UF_AUTHOR_TYPE"]];
+      $result['ID'][] = $arr["ID"];
     }
 
     return $result;
@@ -33,28 +35,51 @@ class SimpleComp extends CBitrixComponent
 
   private function getListNews()
   {
-    // echo "<pre>";
 
-    $arSelect = ["ID", "IBLOCK_ID", "NAME", "DATE_ACTIVE_FROM", "PROPERTY_" . $this->arParams["AUTHOR"]];
-    $arFilter = ["IBLOCK_ID" => $this->arParams["NEWS_IBLOCK_ID"], "ACTIVE" => "Y"];
+    $arSelect = [
+      "ID",
+      "IBLOCK_ID",
+      "NAME",
+      "DATE_ACTIVE_FROM",
+    ];
+    $arFilter = [
+      "IBLOCK_ID" => $this->arParams["NEWS_IBLOCK_ID"],
+      "PROPERTY_" . $this->arParams["AUTHOR"] => $this->getListAuthor()["ID"],
+      "ACTIVE" => "Y"
+    ];
+
+    //получим свойства из инфоблока новости
+    $arProps = [];
+    CIBlockElement::GetPropertyValuesArray($arProps, $this->arParams["NEWS_IBLOCK_ID"], [], ["CODE" => $this->arParams["AUTHOR"]]);
 
     $res = CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
 
+    //сформируюем массив со свойствами
     while ($arr = $res->Fetch()) {
-      $result[] = $arr;
-      // print_r($arr);
+
+      foreach ($arProps as $key => $prop) {
+
+        if ($arr["ID"] == $key) {
+          $result[] = ["ITEM" => $arr, "PROPERTY_" . $this->arParams["AUTHOR"] => $prop["AUTHOR"]["VALUE"]];
+        }
+
+        continue;
+      }
     }
+
     return $result;
   }
 
   private function getCurrentUser()
   {
+    global $USER;
 
-    if (CUser::IsAuthorized(CUser::GetID())) {
+    if ($USER->IsAuthorized(CUser::GetID())) {
 
       $order = [];
       $tmp = 'sort';
-      $user = CUser::GetList($order, $tmp, ["ID" => CUser::GetID()]);
+      $user =  CUser::GetList($order, $tmp, ["ID" => CUser::GetID()]);
+
       if ($res = $user->Fetch()) {
         return ["ID" => $res["ID"], "LOGIN" => $res["LOGIN"], "UF_AUTHOR_TYPE" => $res["UF_AUTHOR_TYPE"]];
       }
@@ -66,32 +91,40 @@ class SimpleComp extends CBitrixComponent
     $listAuthor =   $this->getListAuthor();
     $listNews = $this->getListNews();
     $currentUser = $this->getCurrentUser();
+    $i = 0;
 
-    foreach ($listAuthor as $key => $author) {
+    foreach ($listAuthor["LIST_AUTHOR"] as $key => $author) {
+
       foreach ($listNews as $news) {
 
-        if (intval($author["ID"]) === intval($news["PROPERTY_AUTHOR_VALUE"]) && $currentUser["UF_AUTHOR_TYPE"] == $author["UF_AUTHOR_TYPE"]) {
-          $result[$key]["AUTHOR"] = $author;
-          $result[$key]["NEWS"][] = $news;
+        if (in_array($currentUser["ID"], $news["PROPERTY_AUTHOR"])) {
+          continue;
+        }
+
+        if ($currentUser["UF_AUTHOR_TYPE"] == $author["UF_AUTHOR_TYPE"] && in_array($author["ID"], $news["PROPERTY_AUTHOR"])) {
+          $result["NEWS"][$key]["AUTHOR"] = $author;
+          $result["NEWS"][$key]["NEWS"][] = $news["ITEM"];
+          $result["COUNT"] = $i++;
         }
       }
     }
+
     return $result;
   }
 
   public function executeComponent()
   {
-    global $USER,
-      $APPLICATION;
+    global $APPLICATION;
 
     if (!Loader::includeModule('iblock')) {
       ShowError(GetMessage("SIMPLECOMP_EXAM2_IBLOCK_MODULE_NONE"));
       return;
     }
 
-    if ($this->startResultCache()) {
-      $count = 0;
-      $this->arResult["LIST_NEWS"] = $this->resultFormation();
+    if ($this->startResultCache(false, CUser::GetID())) {
+      $count = $this->resultFormation()["COUNT"];
+      $this->arResult["LIST_NEWS"] = $this->resultFormation()["NEWS"];
+
       $this->includeComponentTemplate();
     } else {
       $this->abortResultCache();
--- local/components/ex2/simplcomp.exam/templates/.default/template.php
+++ local/components/ex2/simplcomp.exam/templates/.default/template.php
@@ -1,8 +1,9 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <p><b><?= GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE") ?></b></p>
 
-<? foreach ($arResult["LIST_NEWS"] as $result) : ?>
-  <ul>
+<ul>
+  <? foreach ($arResult["LIST_NEWS"] as $result) : ?>
+
     <li>
       [<?= $result["AUTHOR"]["ID"]; ?>] -
       <?= $result["AUTHOR"]["LOGIN"]; ?>
@@ -10,17 +11,11 @@
         <? foreach ($result["NEWS"] as $news) : ?>
           <li> - <?= $news["NAME"] ?> - <?= $news["DATE_ACTIVE_FROM"] ?> </li>
     </li>
+
   <? endforeach; ?>
 
-  </ul>
-
-
-  </li>
-  </ul>
+</ul>
+</li>
 
 <? endforeach; ?>
-<pre>
-<!-- <?
-      print_r($arResult);
-      ?>
-</pre> -->
+</ul>
