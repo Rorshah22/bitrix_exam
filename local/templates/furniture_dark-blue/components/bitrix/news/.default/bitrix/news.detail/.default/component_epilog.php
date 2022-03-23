e565e8986dd42dfcb7ea718201b761d56a0fa739
Searhei Khalimon <s.khalimon@manao-team.com>
2022-03-23 09:38:48 +0300

    fix: GET request in context

--- local/templates/furniture_dark-blue/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
+++ local/templates/furniture_dark-blue/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
@@ -1,10 +1,15 @@
 <?
-if (isset($_GET["ID"]) && intval($_GET["ID"]) !== 0) {
+
+use Bitrix\Main\Application;
+
+$request = Application::getInstance()->getContext()->getRequest();
+$id = $request->getQuery('ID');
+
+if (isset($id) && intval($id) !== 0) {
   require($_SERVER["DOCUMENT_ROOT"] . "/local/components/custom/news.detail/ajax.php");
 
-  settype($_GET["ID"], 'integer');
-
-  $arGet = ['id' => $_GET["ID"]];
+  settype($id, 'integer');
+  $arGet = ['id' => $id];
 
   $response = new ResponseReportController();
   $result = $response->reportAction($arGet);
